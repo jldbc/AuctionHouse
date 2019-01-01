f85979569e9a800c1326655a9545982f2361e0ab
James LeDoux <ledouxja@bc.edu>
2019-01-01 16:38:59 -0500

    Stop using debug window and show timestamps and sizes (#1)
    * refactoring
    
    * add creative sizes to output

--- background.js
+++ background.js
@@ -3,18 +3,5 @@
 // found in the LICENSE file.
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.debugger.attach({tabId:tab.id}, version,
-      onAttach.bind(null, tab.id));
+  chrome.windows.create({url: "headers.html?" + tab.id, type: "popup", width: 800, height: 600});
 });
-
-var version = "1.0";
-
-function onAttach(tabId) {
-  if (chrome.runtime.lastError) {
-    alert(chrome.runtime.lastError.message);
-    return;
-  }
-
-  chrome.windows.create(
-      {url: "headers.html?" + tabId, type: "popup", width: 800, height: 600});
-}
--- headers.js
+++ headers.js
@@ -1,51 +1,79 @@
-//disclaimer: I don't know javascript and am figuring this out as I go
-
 var tabId = parseInt(window.location.search.substring(1));
+var filters = { urls: ["<all_urls>"], tabId: tabId };
 
-window.addEventListener("load", function() {
-  chrome.debugger.sendCommand({tabId:tabId}, "Network.enable");
-  chrome.debugger.onEvent.addListener(onEvent);
-});
-
-window.addEventListener("unload", function() {
-  chrome.debugger.detach({tabId:tabId});
-});
-
-var requests = {};
-
-//get browser's url to keep log of where the bids are taking place
-//function getUrl() {
-//      chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
-//      var url = tabs[0].url;
-//      console.log(url);
-//  });
-//}
-
-// todo: can I get the ad size?
-
-function onEvent(debuggeeId, message, params) {
-  if (tabId != debuggeeId.tabId)
-    return;
-
-  if (message == "Network.requestWillBeSent") {
-    var requestDiv = requests[params.requestId];
-    if (!requestDiv) {
-      var requestDiv = document.createElement("div");
-      requestDiv.className = "request";
-      requests[params.requestId] = requestDiv;
-      var urlLine = document.createElement("div");
-      urlLine.textContent = params.request.url;
-      var bids = urlLine.textContent.match(/hb_pb_.*?%3D(\d*\.?\d+)/g);
-      if(bids){
-        var output = bids.map(function(e) { 
-          e = e.replace('%3D', ': '); 
-          e = e.replace('hb_pb_', '');
-          return e;
-        });
-        requestDiv.append(output);
-        document.getElementById("container").appendChild(requestDiv);
-      }
-    }
-    var requestLine = document.createElement("div");
-  }
+function addListeners() {
+    chrome.webRequest.onBeforeRequest.addListener(handleEvent, filters, ['requestBody']);
 }
+
+function removeListeners() {
+    chrome.webRequest.onBeforeRequest.removeListener(handleEvent);
+}
+
+function myFunction(tablink) {
+  // do stuff here
+  console.log(tablink);
+  return tablink;
+}
+
+
+function formatURL(postData) {
+    var bids = postData.match(/hb_pb_.*?%3D(\d*\.?\d+)/g);
+    if(bids){
+      var price_text = bids.map(function(e) { 
+        e = e.replace('%3D', ' price: '); 
+        e = e.replace('hb_pb_', '');
+        e = e.toString();
+        return e;
+      });
+    price_text = price_text.toString();
+    }
+    var sizes = postData.match(/hb_size_.*?%3D(\d*x?\d+)/g);
+    if(sizes){
+      var size_text = sizes.map(function(e) { 
+        e = e.replace('%3D', ' creative size: '); 
+        e = e.replace('hb_size_', '');
+        e = e.toString();
+        return e;
+      });
+    size_text = size_text.toString();
+    }
+    /*// get the current page's url (currently either undefined or the popup url, not working as intended)
+    var current_url = chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      console.log(tabs[0].url);
+      return tabs[0].url;
+   }
+  );*/
+    if(price_text && size_text){
+      text = Date.now().toString() + ", " + size_text + "," + price_text;
+      text = text.replace(',', ', ');
+      return text;
+    }
+    else if (price_text){
+      text = Date.now().toString() + "," + price_text;
+      text = text.replace(',', ', ');
+      return text;  
+    }
+    else if (size_text){
+      text = Date.now().toString() + ", " + size_text;
+      text = text.replace(',', ', ');
+      return text;  
+    }
+}
+
+function handleEvent(details) {
+    var addressDiv = $('div.address[id="req-' + details.requestId + '"]');
+    if (addressDiv.length === 0) {
+        addressDiv = $('<div>').addClass("address").attr("id", "req-" + details.requestId);
+        $("#container").append(addressDiv);
+        var formatted_url = formatURL(details.url);
+        if(formatted_url){
+          $('<div>').addClass("url").text(formatted_url).appendTo(addressDiv);
+        }
+    }
+}
+
+$(function() {
+    addListeners();
+});
+
