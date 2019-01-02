f42f4a812f82590477763e19d79a48bf08863167
James LeDoux <ledouxja@bc.edu>
2019-01-01 20:43:56 -0500

    Add url to output (#2)
    * first attempt at getting url, has async issues
    
    * get url of open browser window
    
    * include comment about order of execution

--- headers.js
+++ headers.js
@@ -1,3 +1,18 @@
+/*
+This script reads through all requests. For each request it runs a promise chain that first
+queries chrome to find the open browser window, then passes this to a formatURL function that
+regexes through the request to identify whether it contains bids and form the output that this 
+passes to the popup window. 
+
+The output should be:
+  timestamp, url, ad sizes and bid prices for all prebid bids
+
+TODO
+- find an easy way to avoid running this whole promise chain for requests without bids 
+- find a simpler way of doing this in general. I'm brand new to writing async code
+*/
+
+
 var tabId = parseInt(window.location.search.substring(1));
 var filters = { urls: ["<all_urls>"], tabId: tabId };
 
@@ -11,12 +26,34 @@ function removeListeners() {
 
 function myFunction(tablink) {
   // do stuff here
-  console.log(tablink);
   return tablink;
 }
 
+function getCurrentURL(callback, postData, addressDiv){ //Take a callback
+    var theTab;
+    var queryInfo = {
+        active: true,
+        //currentWindow: false
+        lastFocusedWindow: true
+      };
+    return new Promise(function(resolve, reject) {
+      chrome.tabs.query(queryInfo,function(tab){
+          theTab = callback(tab); //call the callback with argument
+          resolve([theTab, postData, addressDiv]);
+        });
+    });
+}
 
-function formatURL(postData) {
+function _displayTab(tab){ //define your callback function
+      var my_url = tab[0].url;
+      return my_url;
+}
+
+function displayOutput(addressDiv, formatted_output){ //define your callback function
+  $('<div>').addClass("url").text(formatted_output).appendTo(addressDiv);
+}
+
+function formatURL(postData, my_url, addressDiv) {
     var bids = postData.match(/hb_pb_.*?%3D(\d*\.?\d+)/g);
     if(bids){
       var price_text = bids.map(function(e) { 
@@ -25,8 +62,9 @@ function formatURL(postData) {
         e = e.toString();
         return e;
       });
+
     price_text = price_text.toString();
-    }
+
     var sizes = postData.match(/hb_size_.*?%3D(\d*x?\d+)/g);
     if(sizes){
       var size_text = sizes.map(function(e) { 
@@ -37,28 +75,31 @@ function formatURL(postData) {
       });
     size_text = size_text.toString();
     }
-    /*// get the current page's url (currently either undefined or the popup url, not working as intended)
-    var current_url = chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      console.log(tabs[0].url);
-      return tabs[0].url;
-   }
-  );*/
-    if(price_text && size_text){
-      text = Date.now().toString() + ", " + size_text + "," + price_text;
-      text = text.replace(',', ', ');
-      return text;
-    }
-    else if (price_text){
-      text = Date.now().toString() + "," + price_text;
-      text = text.replace(',', ', ');
-      return text;  
-    }
-    else if (size_text){
-      text = Date.now().toString() + ", " + size_text;
-      text = text.replace(',', ', ');
-      return text;  
-    }
+    return new Promise(function(resolve, reject){
+      if(price_text && size_text){
+        text = size_text + "," + price_text;
+        text = text.replace(',', ', ');
+      }
+      else if (price_text){
+        text = price_text;
+        text = text.replace(',', ', ');
+      }
+      text = Date.now().toString() + "," + my_url + ", " + text;
+      resolve([text, addressDiv]);
+    })
+  }
+}
+
+function async_formatURL(postData, addressDiv) {
+  // make this promise chain a promise itself, so I can wait until it's complete to write the result
+  return new Promise(function(resolve, reject){ 
+    var result = getCurrentURL(_displayTab, postData, addressDiv).then(function(result){
+      return formatURL(result[1], result[0], result[2]);
+    }).then(function(result){
+      return result;
+    });
+    resolve(result);
+  });
 }
 
 function handleEvent(details) {
@@ -66,10 +107,10 @@ function handleEvent(details) {
     if (addressDiv.length === 0) {
         addressDiv = $('<div>').addClass("address").attr("id", "req-" + details.requestId);
         $("#container").append(addressDiv);
-        var formatted_url = formatURL(details.url);
-        if(formatted_url){
-          $('<div>').addClass("url").text(formatted_url).appendTo(addressDiv);
-        }
+
+        async_formatURL(details.url, addressDiv).then(function(result){
+        displayOutput(result[1], result[0]);
+        });
     }
 }
 
