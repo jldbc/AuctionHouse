9ae4a0596863415a74572965fda9f10b130574e5
James LeDoux <jledoux@jamess-macbook.lan>
2019-01-13 10:27:23 -0500

    oops

--- headers.js
+++ headers.js
@@ -95,10 +95,6 @@ function formatURL(postData, my_url, addressDiv) {
       json_text = "{_ts:" + Date.now().toString() + "," + "url:" + my_url + "," + text + '}';
       json_text = json_text.replace(/([.\/\-a\.-zA-Z 0-9-]+):([.\/\-?&_=\+a-zA-Z 0-9-a-zA-Z0-9-]+)/g, "\"$1\":\"$2\"");
       json_text = json_text.replace(/( )/g, '');
-<<<<<<< HEAD
-=======
-      console.log(json_text);
->>>>>>> 27e6ed23005eccb3d6c3d65284b91f76bf23a713
       json_text = JSON.parse(json_text);
       text = "url: " + my_url + "," + text;
       resolve([text, addressDiv, json_text]);
